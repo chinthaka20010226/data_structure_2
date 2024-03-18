c9026ff0d1c13521e9f25d3ca7370ca3b0615e28
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-03-18 23:45:51 +0530

    Factorial Not using Recursion

--- new_/Example.java
+++ new_/Example.java
@@ -2,20 +2,29 @@ import java.util.*;
 
 public class Example {
     public static int fact(int x){
-        if(x < 0){
-            System.out.println("Input number is invalid");
-            return 0;
+        int fact = 1;
+
+        while(x > 0){
+            fact = fact * x;
+            x = x - 1;
         }
-        return x;
+
+        return fact;
     }
 
     public static void main(String[] args) {
         // System.out.println("Hello world !");
 
         Scanner input = new Scanner(System.in);
+
         System.out.println("Input the number : ");
         int number = input.nextInt();
 
-        System.out.println("Result is "+fact(number));
+        if(number >= 0){
+            System.out.println("Result is "+fact(number));
+        }
+        else{
+            System.out.println("Input number is Invalid");
+        }
     }
 }
