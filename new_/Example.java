5ec7499cdc15c15d6214467a013b83f0756d7463
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-03-20 21:47:14 +0530

    Binary Search Tree without the Recursion

--- new_/Example.java
+++ new_/Example.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+
+/*
 public class Example {
     public static int fact(int x){
         int fact = 1;
@@ -26,5 +28,105 @@ public class Example {
         else{
             System.out.println("Input number is Invalid");
         }
+    }
+}
+*/
+
+
+
+
+
+
+
+class TreeNode{
+    int key;
+    TreeNode left,right;
+
+    public TreeNode(int key){
+        this.key = key;
+        left = right = null;
+    }
+}
+
+
+class BinarySearchTree{
+    TreeNode root;
+
+    public BinarySearchTree(){
+        root = null;
+    }
+
+    public void insert(int key){
+
+        TreeNode node = new TreeNode(key);
+        
+        if(root == null){
+            System.out.println("Insert value to the Root");
+            root = node;
+            return;
+        }
+
+        TreeNode current = root;
+
+        while(true){
+            if(current.key > key){
+                if(current.left == null){
+                    System.out.println("Insert value to the left of " + current.key);
+                    current.left = node;
+                    return;
+                }
+                else{
+                    current = current.left;
+                }
+            }
+            else if(current.key < key){
+                if(current.right == null){
+                    System.out.println("Insert value to the right of " + current.key);
+                    current.right = node;
+                    return;
+                }
+                else{
+                    current = current.right;
+                }
+            }
+            else{
+                System.out.println("This value already inserted");
+                return;
+            }
+        }
+
+    }
+
+
+    public boolean search(int val){
+        TreeNode current = root;
+
+        while(true){
+            if(current.key > val){
+                System.out.println("Searching value from left of " + current.key);
+                current = current.left;
+            }
+            else if(current.key < val){
+                System.out.println("Searching value from right of " + current.key);
+                current = current.right;
+            }
+            else{
+                return true;
+            }
+        }
+    }
+}
+
+
+public class Example{
+    public static void main(String args[]){
+        BinarySearchTree bst = new BinarySearchTree();
+
+        bst.insert(8);
+        bst.insert(6);
+        bst.insert(3);
+        bst.insert(12);
+
+        System.out.println("Searching vlaue is " + bst.search(3));
     }
 }
